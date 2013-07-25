42df3dd0e5233d391525bd7fd4a501c76a0662b9
Greg Nicholas <greg.nicholas@gmail.com>
2013-07-25 09:35:49 -0400

    create behavior scatterplot skeleton

--- controllers.js
+++ controllers.js
@@ -2,6 +2,10 @@ function BaseController($scope, moment, baseViewState) {
     $scope.user_fullname = "Jack Donaghy";
     $scope.date = moment().format('MMMM Do');
     $scope.baseViewState = baseViewState;
+
+    $scope.showBehaviorChart = function() {
+    	baseViewState.currentView = 'behaviorChart';
+    };
 }
 
 function SidebarController($scope, studentService, baseViewState) {
@@ -11,4 +15,18 @@ function SidebarController($scope, studentService, baseViewState) {
         baseViewState.currentStudent = student;
         baseViewState.currentView = 'chartMenu';
     };
+}
+
+function ScatterplotController($scope, baseViewState, behaviorIncidentService) {
+	$scope.student = baseViewState.currentStudent;
+	$scope.incidents = behaviorIncidentService.incidentsForStudent($scope.student, false);
+	$scope.types = behaviorIncidentService.typesForStudent($scope.student, false);
+
+	$scope.createIncident = function(type) {
+		$scope.incidents.createIncident(type, new Date());
+	};
+
+	$scope.dateFormat = function(date) {
+		return moment(date).format('MM/DD/YY HH:mm');
+	};
 }
