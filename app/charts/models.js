73d7cf2b18e4efd62eed0189eb58d023fb31f908
Greg Nicholas <greg.nicholas@gmail.com>
2013-09-17 13:41:36 -0400

    add metric and phase line data to skeleton

--- app/charts/models.js
+++ app/charts/models.js
@@ -1,22 +1,72 @@
 /**
  * Chart model definition
- *
- * To be fleshed out with more DayMetric-related data handling. For now
- * just handling the label.
  */
-angular.module('plea').factory('Chart', function(Backbone) {
+angular.module('plea').factory('Chart', function(Backbone, dayMetricDataStore, phaseLineDataStore) {
 	return Backbone.Model.extend({
 		urlRoot: '/plea/charts/',
 		getLabel: function() {
 			return this.get('subtopic').name +
 					" (" + this.get('inputChannel').name + "/" +
 					this.get('outputChannel').name + ")";
+		},
+
+		/**
+		 * Add a new daily record (set of metrics) to the chart.
+		 * @param {DailyRecord} DailyRecord model instance
+		 */
+		addDayMetric: function(dailyRecord) {
+			console.warn('addDayMetric stub called');
+		},
+
+		/**
+		 * Returns a Collection of DayMetrics instances sorted by date.
+		 * @param {Function} asyncCallback Post-async fetch callback.
+		 * @return {Collection}
+		 */
+		getDayMetrics: function(asyncCallback) {
+			return dayMetricDataStore.getAllForChart(this, asyncCallback);
+		},
+
+		/**
+		 * Add a phase line.
+		 * @param {PhaseLine} PhaseLine model instance
+		 */
+		addPhaseLine: function(phaseLine) {
+			console.warn('addDayMetric stub called');
+		},
+
+		/**
+		 * Returns a Collection of PhaseLine instances sorted by date.
+		 * @param {Function} asyncCallback	Post-async fetch callback.
+		 * @return {Collection}
+		 */
+		getPhaseLines: function(asyncCallback) {
+			return phaseLineDataStore.getAllForChart(this, asyncCallback);
 		}
+
 	});
 });
 
 /**
- * Data store for chart objects.
+ * DayMetric model definition
+ */
+angular.module('plea').factory('DayMetric', function(Backbone) {
+	return Backbone.Model.extend({
+		urlRoot: '/plea/daymetrics/'
+	});
+});
+
+/**
+ * PhaseLine definition
+ */
+angular.module('plea').factory('PhaseLine', function(Backbone) {
+	return Backbone.Model.extend({
+		urlRoot: '/plea/phaselines/'
+	});
+});
+
+/**
+ * Data store for Chart objects.
  *
  * Provided accessors:
  * - getAllForStudent: Returns all charts for a given student
@@ -29,38 +79,118 @@ angular.module('plea').factory('chartDataStore', function(Chart) {
 		}))();
 	};
 
-	// will be a map of student ids to {promise: <obj>, collection: <obj>} objects
-	var cache = {};
-
 	return {
 		/**
 		 * Returns a Collection of all Charts related to the given student
-		 * model. Makes asynchronous call to fetch data on the first
-		 * invocation of this method; use the callback argument for any
-		 * code that is dependent on the results of this call.
+		 * model. Makes asynchronous call to fetch data; use the callback
+		 * argument for any code that is dependent on the results of this
+		 * call.
 		 *
 		 * @param  {Student}  student
 		 * @param  {Function} syncCallback		Callback function to run after collection's
-		 *                                      successful initial sync to the server. Callback
+		 *                                      successful sync to the server. Callback
 		 *                                      takes one argument: the collection.
 		 * @return {Collection}
 		 */
 		getAllForStudent: function(student, syncCallback) {
-			var studentCache = cache[student.id];
-			if (!studentCache) {
-				studentCache = cache[student.id] = {};
-				studentCache.collection = studentChartCollectionFactory(student);
-				studentCache.promise = studentCache.collection.fetch();
-			}
+			var collection = studentChartCollectionFactory(student);
+			var promise = collection.fetch();
 
 			if (syncCallback) {
-				studentCache.promise.success(function() {
-					syncCallback(studentCache.collection);
+				promise.success(function() {
+					syncCallback(collection);
 				});
 			}
 
-			return studentCache.collection;
+			return collection;
 		}
 
 	};
 });
+
+/**
+ * Data store for DayMetric objects.
+ *
+ * Provided accessors:
+ * - getAllForChart: Returns all DayMetrics for a given chart
+ */
+angular.module('plea').factory('dayMetricDataStore', function(DayMetric) {
+	var chartDayMetricCollectionFactory = function(chart) {
+		return new (Backbone.Collection.extend({
+			url: chart.get('dayMetricsUrl'),
+			model: DayMetric
+		}))();
+	};
+
+	return {
+		/**
+		 * Returns a Collection of all DayMetrics related to the given Chart
+		 * model. Makes asynchronous call to fetch data; use the callback
+		 * argument for any code that is dependent on the results of this
+		 * call.
+		 *
+		 * @param  {Chart}  chart
+		 * @param  {Function} syncCallback		Callback function to run after collection's
+		 *                                      successful sync to the server. Callback
+		 *                                      takes one argument: the collection.
+		 * @return {Collection}
+		 */
+		getAllForChart: function(chart, syncCallback) {
+			var collection = chartDayMetricCollectionFactory(chart);
+			var promise = collection.fetch();
+
+			if (syncCallback) {
+				promise.success(function() {
+					syncCallback(collection);
+				});
+			}
+
+			return collection;
+		}
+
+	};
+});
+
+/**
+ * Data store for PhaseLine objects.
+ *
+ * Provided accessors:
+ * - getAllForChart: Returns all PhaseLines for a given chart
+ */
+angular.module('plea').factory('phaseLineDataStore', function(PhaseLine) {
+	var chartPhaseLineCollectionFactory = function(chart) {
+		return new (Backbone.Collection.extend({
+			url: chart.get('phaseLinesUrl'),
+			model: PhaseLine
+		}))();
+	};
+
+	return {
+		/**
+		 * Returns a Collection of all PhaseLine related to the given Chart
+		 * model. Makes asynchronous call to fetch data; use the callback
+		 * argument for any code that is dependent on the results of this
+		 * call.
+		 *
+		 * @param  {Chart}  chart
+		 * @param  {Function} syncCallback		Callback function to run after collection's
+		 *                                      successful sync to the server. Callback
+		 *                                      takes one argument: the collection.
+		 * @return {Collection}
+		 */
+		getAllForChart: function(chart, syncCallback) {
+			var collection = chartPhaseLineCollectionFactory(chart);
+			var promise = collection.fetch();
+
+			if (syncCallback) {
+				promise.success(function() {
+					syncCallback(collection);
+				});
+			}
+
+			return collection;
+		}
+
+	};
+});
+
--- app/main/controllers/chartCtrl.js
+++ app/main/controllers/chartCtrl.js
@@ -5,15 +5,22 @@
 angular.module('plea').controller('ChartCtrl', function($scope, mainViewState) {
     $scope.chart = mainViewState.selectedChart;
 
+    $scope.dayMetricCollection = $scope.chart.getDayMetrics();
+    $scope.phaseLineCollection = $scope.chart.getPhaseLines();
+
 	// Note that this controller will be re-invoked every time a chart is
 	// selected because its under an ng-if clause. That's why we can get
-	// away with settings $scope.chart once and not watching for mainViewState
+	// away with setting $scope.chart once and not watching for mainViewState
 	// to trigger a 'change:selectedChart' event. This is not true in general
 	// (for example, in `chartSelectionCtrl` we need to explicitly watch for
 	// a change).
 	//
-	// TODO: the proper way to implement this (and any other controller whose
-	// state depends on some externally-mutable injected value) is probably to
-	// use a true directive, but this is a major refactor that's not necessary
+	// TODO: There's two (more) proper ways to implement this:
+	//
+	// 1. If re-initialization on every chart load is desirable, consider
+	// using a true directive. This is a major refactor that's not necessary
 	// at the moment
+	// 2. More feasibly: Just take the philosophy that all controllers are
+	// initialized on app load with no bound data, and state triggers are
+	// always necessary for setting up the state
 });
