97f9048f053420fb2b28f3a06b0e39119d6f6c3f
Greg Nicholas <greg.nicholas@gmail.com>
2013-09-17 15:28:45 -0400

    generalize date filter

--- app/main/filters.js
+++ app/main/filters.js
@@ -1,5 +1,12 @@
-angular.module('plea').filter('formatDatetime', function(moment) {
-	return function(date) {
-		return moment(date).format('lll');
-	};
-});
+angular.module('plea').
+	/**
+	 * Simple date filterer that defers to Moment.js formatter
+	 */
+	filter('formatDate', function(moment) {
+		return function(date, fmt) {
+			if (date) {
+				return moment(date).format(fmt);
+			}
+			return '';
+		};
+	});
