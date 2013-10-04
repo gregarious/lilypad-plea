a62948124f886bc73bab0c04cea34a9b671fa6b3
Arthur Hong <arthurrhong@gmail.com>
2013-10-04 00:37:54 -0400

    generates chart and pulls data

--- app/main/controllers/chartCtrl.js
+++ app/main/controllers/chartCtrl.js
@@ -6,9 +6,9 @@ angular.module('plea').controller('ChartCtrl', function($scope, mainViewState, _
     /**
      * $scope initialization
      */
-
     $scope.chart = mainViewState.selectedChart;
-
+    _a = $scope;
+    var a = $scope;
     // upon fetching data, this array will be filled with bare Javascript
     // objects of the form: {date: '2013-09-03', floor: 40, correct: 40, etc.}
     $scope.dailyRecords = [];
@@ -23,6 +23,8 @@ angular.module('plea').controller('ChartCtrl', function($scope, mainViewState, _
 			var pkg = packageMetricValues(dayMetricCollection.where({'date': date}));
 			$scope.dailyRecords.push(pkg);
 		});
+
+		_chart = new Chart();
     });
 
     // No need to do any extra processing to the phase lines, just expose
@@ -58,5 +60,457 @@ angular.module('plea').controller('ChartCtrl', function($scope, mainViewState, _
 			}
 		});
 		return pkg;
+	}
+
+	/**
+	 * Celeration Chart
+	 */
+
+	function Chart() {
+		this.numberOfDays = 140;
+		this.minExponent = -3;
+		this.maxExponent = 3;
+		this.activeDay = 1;
+		//a decade is the section between two exponents of ten on the chart
+		//for example, a decade could be from 1-10 or 0.01-0.1
+		this.numberOfDecades = Math.abs(this.minExponent) + Math.abs(this.maxExponent);
+		this.drawArea = document.getElementById('draw');
+		this.init();
+	}
+
+	Chart.prototype.init = function() {
+		this.setDimensions();
+		this.createPaperDrawingArea();
+		this.setObjects();
+		this.setStyles();
+		this.drawHistoricalData(a.dailyRecords);
+		this.drawXAxis();
+		this.drawYAxis();
+	}
+
+	Chart.prototype.setDimensions = function() {
+		var width = window.innerWidth;
+		var height = window.innerHeight;
+
+		//specify margins based on width and height of window
+		this.bottomMargin = height * 0.05;
+		this.topMargin = height * 0.05;
+		this.leftMargin = width * 0.05;
+		this.rightMargin = width * 0.05;
+
+		//initialize width and height of chart based on window dimensions
+		this.chartWidth = width - (this.leftMargin + this.rightMargin);
+		this.chartHeight = height - (this.topMargin + this.bottomMargin);
+
+		//set padding for number labels on chart
+		this.labelPadding = this.leftMargin * 0.15;
+
+		//set tickmark lengths
+		this.baseTickLength = 8;
+		this.intermediateTickLength = 5;
+
+		//calculate height of a decade in pixels
+		this.decadeHeight = this.chartHeight / this.numberOfDecades;
+	}
+
+	//initialize raphael.js 'paper' drawing area
+	Chart.prototype.createPaperDrawingArea = function() {
+		var that = this;
+		var width = window.innerWidth;
+		var height = window.innerHeight;
+		this.paper = new Raphael(that.drawArea, width, height);
+	}
+
+	Chart.prototype.setObjects = function() {
+		this.metric = {
+			'corrects' : {
+							'marker' : 'filled-circle',
+							'metric' : null // instance of metric is stored here
+						 },
+			'floor' :   {
+							'marker' : 'line',
+							'metric' : null
+					     },
+			'errors' :   {
+							'marker' : 'cross',
+							'metric' : null
+					     },
+			'trials' :   {
+							'marker' : 'empty-circle',
+							'metric' : null
+					     }
+		};
+
+		this.phaseline = {
+			'phaseline' : null
+		};
+	}
+
+	Chart.prototype.setStyles = function() {
+		this.metricStyles = {
+			'filled-circle' : 	{
+									'fill-opacity': 1,
+							   		'fill': '#7FFF00',
+							   		'stroke': 0,
+							   		'opacity': .9
+							   	},
+			'line' : 			{ 
+									'stroke-width': '1',
+						 			'stroke': '#000'
+						 		},
+			'cross' : 			{ 
+									'stroke-width': '1',
+					    			'stroke': "#F62817",
+					    			'opacity': .9
+					    		},
+			'empty-circle' : 	{
+									'fill-opacity': 0
+								}
+		};
+		this.phaselineStyles = {
+			'phaseline' : 		{
+									'stroke-width' : 1.5,
+									'stroke' : '#404040'
+								},
+			'phaseline-floor' : {
+									'text-anchor' : 'start'
+								},
+			'phaseline-note' : 	{
+									'text-anchor' : 'middle'
+								}
+		}
+		this.chartStyles = {
+			//horizontal lines and labels
+			'decadebaseline' : {
+				'weight' : '1.1',
+				'color' : '#A6C5D3'
+			},
+			'decadebaselabel' : {
+				'font-size' : 15,
+				'text-anchor' : 'end',
+				'color' : '#A6C5D3'
+			},
+			'intermediateline' : {
+				'weight' : '0.4',
+				'color' : '#A6C5D3'
+			},
+			'middleline' : {
+				'weight' : '0.8',
+				'color' : '#A6C5D3'
+			},
+			'middlelabel' : {
+				'font-size' : 10,
+				'text-anchor' : 'end',
+				'color' : '#A6C5D3'
+			},
+			'floorlabel' : {
+				'font-size' : 10,
+				'text-anchor' : 'start',
+				'color' : '#A6C5D3'
+			},
+			// vertical lines and labels
+			'weekline' : {
+				'weight' : '1',
+				'color' : '#A6C5D3'
+			},
+			'weeklabel' : {
+				'font-size' : 14,
+				'text-anchor' : 'middle',
+				'color' : '#A6C5D3'
+			},
+			'activedayline' : {
+				'weight' : '1',
+				'color' : '#0000FF'
+			},
+			'activedaylabel' : {
+				'font-size' : 8,
+				'text-anchor' : 'middle',
+				'color' : '#0000FF'
+			}
+		}
+	}
+
+	Chart.prototype.drawXAxis = function() {
+		var lineStartX = this.leftMargin;
+		var lineEndX = this.chartWidth;
+		var chartBottomY = this.topMargin + this.chartHeight;
+
+		//for each decade, draw the lines within that decade on the log scale
+		for (var i = 0; i < this.numberOfDecades; i++) {
+			//base values are multiples of ten
+			var decadeBaseValue = Math.pow(10, this.minExponent + i);
+
+			//find y position (pixels) for the base value of the decade
+			var decadeNumber = i;
+			var decadeBasePosition = chartBottomY - (decadeNumber*this.decadeHeight);
+
+			//draw the baseline's value label
+			this.drawLabel(lineStartX - this.labelPadding, decadeBasePosition, decadeBaseValue, this.chartStyles['decadebaselabel']);
+
+			//draw floor label for baseline
+			var labelText;
+			var floorPadding = 70;
+			if (decadeBaseValue <= 1) labelText = 1/decadeBaseValue + "'";
+			else labelText = "";
+			var endX = lineEndX + 1.7*this.baseTickLength;
+			if (labelText === "") endX = lineEndX + this.baseTickLength;
+			this.drawLabel(lineEndX + floorPadding, decadeBasePosition, labelText, this.chartStyles['floorlabel']);
+			this.drawHorizontalLine(lineStartX - this.baseTickLength, endX, decadeBasePosition, this.chartStyles['decadebaseline']);
+
+			//if we're on last decade, draw a top baseline and its label
+			if (decadeNumber === (this.numberOfDecades - 1)) {
+				var topDecadePosition = decadeBasePosition - this.decadeHeight;
+				var topDecadeValue = Math.pow(10, this.minExponent + i + 1);
+				this.drawHorizontalLine(lineStartX - this.baseTickLength, lineEndX + this.baseTickLength, topDecadePosition, this.chartStyles['decadebaseline']);
+				this.drawLabel(lineStartX - this.labelPadding, topDecadePosition, topDecadeValue, this.chartStyles['decadebaselabel']);
+			} 
+
+			//draw all lines in between baselines
+			this.drawIntermediateLines(decadeNumber, decadeBaseValue, decadeBasePosition, lineStartX, lineEndX);
+		}
+	}
+
+	Chart.prototype.drawYAxis = function() {
+		var spacing = this.chartWidth/this.numberOfDays;
+
+		var lineStartY = this.topMargin;
+		var lineEndY = lineStartY + this.chartHeight;
+
+		var startX = this.leftMargin;
+		var vertLabelPadding = this.labelPadding + 10;
+
+		for (var i = 0; i <= this.numberOfDays; i++) {
+			//draw blue line representing today's line
+			if (i === this.activeDay) {
+				this.drawLabel(startX + i*spacing, lineStartY - this.labelPadding, 'TODAY', this.chartStyles['activedaylabel']);
+				this.drawVerticalLine(startX + i*spacing, lineStartY, lineEndY, this.chartStyles['activedayline'], true, i);
+			}
+
+			else {
+				//every 7th vertical line is bolded
+				if (i%7 === 0) {
+					//include tickmarks and labels on the 14th line
+					if (i%14 === 0) {
+						this.drawLabel(startX + i*spacing, lineEndY + vertLabelPadding, i, this.chartStyles['weeklabel']);
+						this.drawVerticalLine(startX + i*spacing, lineStartY - this.baseTickLength, lineEndY + this.baseTickLength, this.chartStyles['weekline']);
+					}
+					else {
+						this.drawVerticalLine(startX + i*spacing, lineStartY, lineEndY, this.chartStyles['weekline']);
+					}
+				}
+
+				//draw normal vertical lines
+				else {
+					this.drawVerticalLine(startX + i*spacing, lineStartY, lineEndY, this.chartStyles['intermediateline']);
+				}
+			}
+		}
+	}
+
+	//draws text (this function is used to draw numbers next to lines)
+	Chart.prototype.drawLabel = function(x, y, lineValue, labelAttr) {
+		var label = this.paper.text(x, y/2+2, lineValue);
+
+		var fontSize = labelAttr['font-size'] || 15;
+		var fontColor = labelAttr['color'] || '#A6C5D3';
+		var textAlign = labelAttr['text-anchor'] || 'start';
+
+		label.attr({
+			'font-size': fontSize,
+			'fill': fontColor,
+			'text-anchor': textAlign
+		});
+	}
+
+	Chart.prototype.drawHorizontalLine = function(x1, x2, y, params) {
+		var deltaY = 0; //zero bc we don't want slanted lines
+		var basePath = "M " + x1 + " " + y + " l " + x2 + " " + deltaY;
+
+		//add line to the drawing area
+		var line = this.paper.path(basePath);
+		var lineColor = params['color'] || '#000';
+		var lineWeight = params['weight'] || '1';
+
+		line.attr({
+			"stroke": lineColor,
+			"stroke-width": lineWeight
+		});
+	}
+
+	//get y positions for and draw lines for values in between the high baseline and the low baseline
+	Chart.prototype.drawIntermediateLines = function(decadeNumber, decadeBaseValue, decadeBasePosition, x1, x2) {
+		for (var j = 2; j < 10; j++) {
+			var lineValue = j * decadeBaseValue;
+			var intermediateLineYPosition = this.valueToYPosition(decadeBasePosition, lineValue, decadeBaseValue);
+			var floorPadding = 70;
+
+			//only draw line with ticks and labels on the fifth line in the decade
+			//(this is just how the chart is designed)
+			if (j === 5) {
+				this.drawLabel(x1 - this.labelPadding, intermediateLineYPosition, lineValue, this.chartStyles['middlelabel']);
+				this.drawHorizontalLine(x1 - this.intermediateTickLength, x2 + this.intermediateTickLength, intermediateLineYPosition, this.chartStyles['middlelabel']);
+				//draw floor label
+				if (lineValue < 1) {
+					this.drawLabel(x2 + floorPadding, intermediateLineYPosition, 1/lineValue + "'", this.chartStyles['floorlabel']);
+					this.drawHorizontalLine(x1 - this.intermediateTickLength, x2 + 2*this.intermediateTickLength, intermediateLineYPosition, this.chartStyles['middleline']);
+				}
+			}
+
+			else {
+				var labelText;
+				if (lineValue >= 1 && lineValue <=6) labelText = 60/lineValue + '"';
+				else if (lineValue === .002 || lineValue === .02 || lineValue === .2) labelText = 1/lineValue + "'";
+				else labelText = "";
+				var endX = x2 + this.intermediateTickLength;
+				if (labelText === "") endX = x2
+				this.drawLabel(x2 + floorPadding, intermediateLineYPosition, labelText, this.chartStyles['floorlabel']);
+				this.drawHorizontalLine(x1, endX, intermediateLineYPosition, this.chartStyles['intermediateline']);
+			}
+		}
+	}
+
+	Chart.prototype.drawVerticalLine = function(x, y1, y2, params) {
+		var deltaX = 0; // zero because we don't want the line to be slanted
+		var deltaY = y2 - y1;
+		var basePath = "M " + x + ' ' + y1 + " l " + deltaX + ' ' + deltaY;
+		// add the line to the drawing area
+		var line = this.paper.path(basePath);
+
+		// get attrs to add to line. set to defaults if they're undefined
+		var lineColor = params['color'] || '#000';
+		var lineWeight = params['weight'] || '1';
+
+		line.attr({"stroke-width": lineWeight,
+				   "stroke": lineColor});
+
+	}
+
+	//takes a value and converts it to a y position on the chart
+	Chart.prototype.valueToYPosition = function(baseLineYPosition, lineValue, decadeBaseValue) {
+		var decadeProportion = 1 * lineValue/decadeBaseValue;
+		var logDecadeProportion = log10(decadeProportion);
+		var offsetFromBase = this.decadeHeight * logDecadeProportion;
+		var y = baseLineYPosition - offsetFromBase;
+		return y;
+	}
+
+	// converts a day (in int form, between 0 and 140 and 
+	// returns the x coordinate of that day line on the chart)
+	Chart.prototype.dayToXPosition = function(day) {
+		if ((day < 0) || (day > this.numberOfDays)) {
+			return 'day out of range';
+		}
+		// compute based on margins and spacing. add variables from the drawYaxis function to the
+		// chart object if need be
+		else {
+			var startX = this.leftMargin;
+			var spacing = this.chartWidth/this.numberOfDays;
+			var xValue = startX + day*spacing;
+			return xValue;
+		}
+	}
+
+	Chart.prototype.getDecadeBasePosition = function(decadeNumber) {
+		var chartBottomY = this.topMargin + this.chartHeight;
+		var position = chartBottomY - (decadeNumber * this.decadeHeight);
+		return position;
+	}
+
+	Chart.prototype.getDecadeBaseValue = function(decadeNumber) {
+		var decadeExponent = decadeNumber + this.minExponent;
+		var value = Math.pow(10, decadeExponent);
+		return value;
+	}
+
+	Chart.prototype.drawHistoricalData = function(dailyRecords) {
+		for (var i = 0; i < dailyRecords.length; i++) {
+			var dayPosition = this.dayToXPosition(i+1);
+			if (dailyRecords.corrects !== null) {
+				this.drawHistoricalCorrects(dayPosition, dailyRecords[i].corrects);
+			}
+			if (dailyRecords.errors !== null) {
+				this.drawHistoricalErrors(dayPosition, dailyRecords[i].errors);
+			}
+			if (dailyRecords.trials !== null) {
+				this.drawHistoricalTrials(dayPosition, dailyRecords[i].trials);
+			}
+			if (dailyRecords.floors !== null) {
+				this.drawHistoricalFloors(dayPosition, dailyRecords[i].floor);
+			}
+			this.activeDay++;
+		}
+	}
+
+	Chart.prototype.drawHistoricalCorrects = function(day, value) {
+		var cx = day;
+		var decadeNumber = 3; //default base decade for corrects
+		var decadeBaseValue = this.getDecadeBaseValue(decadeNumber);
+		var decadeBasePosition = this.getDecadeBasePosition(decadeNumber);
+		var cy = this.valueToYPosition(decadeBasePosition, value * decadeBaseValue, decadeBaseValue);
+		var radius = 3;
+		var newCircle = this.paper.circle(cx, cy, radius);
+		newCircle.attr(this.metricStyles['filled-circle']);
+	}
+
+	Chart.prototype.drawHistoricalErrors = function(day, value) {
+		var cx = day;
+		var decadeNumber = 3; //default base decade for errors
+		var decadeBaseValue = this.getDecadeBaseValue(decadeNumber);
+		var decadeBasePosition = this.getDecadeBasePosition(decadeNumber);
+		var cy = this.valueToYPosition(decadeBasePosition, value * decadeBaseValue, decadeBaseValue);
+		var radius = 3;
+
+		// draw cross on chart, cross is drawn as two lines
+		var crossPathOne = 'M '+(cx-radius)+' '+(cy-radius)+' L '+(cx+radius)+' '+(cy+radius);
+		var crossPathTwo = 'M '+(cx-radius)+' '+(cy+radius)+' L '+(cx+radius)+' '+(cy-radius);
+		var crossLineOne = this.paper.path(crossPathOne);
+		var crossLineTwo = this.paper.path(crossPathTwo);
+
+		// set error attributes onto cross
+		var errorMetricStyles = this.metricStyles['cross'];
+		crossLineOne.attr(errorMetricStyles);
+		crossLineTwo.attr(errorMetricStyles);
+	}
+
+	Chart.prototype.drawHistoricalTrials = function(day, value) {
+		var cx = day;
+		var decadeNumber = 0; //default base decade for trials
+		var decadeBaseValue = this.getDecadeBaseValue(decadeNumber);
+		var decadeBasePosition = this.getDecadeBasePosition(decadeNumber);
+		var cy = this.valueToYPosition(decadeBasePosition, value * decadeBaseValue, decadeBaseValue);
+		var radius = 3;
+		var newCircle = this.paper.circle(cx, cy, radius);
+		newCircle.attr(this.metricStyles['empty-circle']);
+	}
+
+	Chart.prototype.drawHistoricalFloors = function(day, value) {
+		var cx = day;
+		console.log(value);
+		var logValue = this.floorToLog(value);
+		console.log(logValue);
+		var decadeNumber = 3; //default base decade for floors
+		var decadeBaseValue = this.getDecadeBaseValue(decadeNumber);
+		var decadeBasePosition = this.getDecadeBasePosition(decadeNumber);
+		var cy = this.valueToYPosition(decadeBasePosition, logValue * decadeBaseValue, decadeBaseValue);
+		var radius = 3;
+
+		console.log(cy);
+		var linePath = 'M '+(cx-radius)+' '+cy+' L '+(cx+radius)+' '+cy;
+		var newLine = this.paper.path(linePath);
+		// set floor attributes onto line
+		var floorMetricStyles = this.metricStyles['line'];
+		newLine.attr(floorMetricStyles);
+
+	return newLine;
+	}
+
+	Chart.prototype.floorToLog = function(floorValue) {
+		//in seconds
+		return 1/(floorValue/60);
+	}
+
+	//helper to do log10 of numbers
+	function log10(value) {
+		return Math.log(value) / Math.LN10;
 	}
 });
---
+++ app/main/controllers/metric.js
@@ -0,0 +1,219 @@
+/* Metric Class */
+
+function Metric(chart, type, day, y) {
+	this.chart = chart;
+	this.type = type;
+	this.marker = chart.getMarkerForMetric(type);
+	this.day = day;
+
+	if (type === 'trials') this.decadeNumber = 0; 
+	else this.decadeNumber = 3;
+
+	this.x = chart.dayToXPosition(day);
+	this.y = y;
+	this.radius = 5;
+
+	this.touchedValue = this.getTouchedValue(); 
+	this.logarithmicValue = this.getLogarithmicValue();// this is necessary to store the logarithmic value for floors
+	this.value = this.getValue();
+	this.paperObject = this.drawMarker();
+}
+
+Metric.prototype.getTouchedValue = function() {
+	var decadeBasePosition = this.decadeNumber * this.chart.decadeHeight;
+	var percentAwayFromVBase = (this.y - decadeBasePosition)/this.chart.decadeHeight;
+	var decadeBaseValue = Math.pow(10, this.decadeNumber + this.chart.minExponent);
+	var value = decadeBaseValue * Math.pow(10, percentAwayFromVBase);
+	var roundedValue = this.getRoundedValue(value);
+	if (this.type === 'trials') {
+		return roundedValue*1000;
+	}
+	else {
+		return roundedValue;
+	}
+}
+
+Metric.prototype.getValue = function() {
+	if (this.type === 'floor') {
+		return this.getFloorValue();
+	}
+	else {
+		return this.getLogarithmicValue();
+	}
+}
+
+Metric.prototype.getFloorValue = function() {
+	// gets value in seconds
+	var snappedLogarithmicValue = this.getSnappedValue();
+	var floorValue = Math.round(60/snappedLogarithmicValue);
+	return floorValue;
+}
+
+Metric.prototype.getLogarithmicValue = function() {
+	var logarithmicValue = this.touchedValue;
+	var snappedLogarithmicValue = this.getSnappedValue();
+	if (this.type === 'corrects' || this.type === 'errors') {
+		if (logarithmicValue > 10) return logarithmicValue;
+		if (logarithmicValue <= 10) return snappedLogarithmicValue;
+	}
+	if (this.type === 'floor' || this.type === 'trials') {
+		return snappedLogarithmicValue;
+	}
+}
+
+Metric.prototype.getRoundedValue = function(value) {
+	if (value >= 1) return Math.round(value);
+	if (value >= 0.1 && value < 1) return Math.round(value*100)/100;
+	if (value >= 0.01 && value < 0.1) return Math.round(value*1000)/1000;
+	if (value >= 0.001 && value < 0.01) return Math.round(value*10000)/10000;
+}
+
+Metric.prototype.getSnappedValue = function() {
+	var snapFactor;
+	if (this.touchedValue >= 0 && this.touchedValue < 0.01) snapFactor = 1000; 
+	if (this.touchedValue >= 0.01 && this.touchedValue < 0.1) snapFactor = 100;
+	if (this.touchedValue >= .1 && this.touchedValue < 1) snapFactor = 10;
+	if (this.touchedValue >= 1 && this.touchedValue < 10) snapFactor = 1;
+	if (this.touchedValue >= 10 && this.touchedValue < 100) snapFactor = .1;
+	if (this.touchedValue >= 100 && this.touchedValue < 1000) snapFactor = .01;
+	snappedLogarithmicValue = Math.round(this.touchedValue*snapFactor) / snapFactor;
+	return snappedLogarithmicValue;
+}
+
+Metric.prototype.drawMarker = function() {
+	var y = this.getMarkerYPosition(this.logarithmicValue);
+	
+	if (this.marker === 'filled-circle') return this.drawCircle(y, true);
+	if (this.marker === 'line') return this.drawLine(y);
+	if (this.marker === 'cross') return this.drawCross(y);
+	if (this.marker === 'empty-circle') return this.drawCircle(y, false);
+}
+
+Metric.prototype.getMarkerYPosition = function(value) {
+	var decadeBasePosition = this.chart.getDecadeBasePosition(this.decadeNumber);
+	var decadeBaseValue = this.chart.getDecadeBaseValue(this.decadeNumber);	
+	var markerY = this.chart.valueToYPosition(decadeBasePosition, value * decadeBaseValue, decadeBaseValue);
+	return markerY;
+}
+
+Metric.prototype.drawCircle = function(y, isFilled) {
+	// draw circle on chart
+	var newCircle = this.chart.paper.circle(this.x, y, this.radius);
+
+	// set corrects attributes onto circle
+	if (isFilled === true) {
+		var correctMetricStyles = this.chart.metricStyles[this.marker];
+		newCircle.attr(correctMetricStyles);
+	}
+
+	// set trials attributes onto circle
+	else {
+		var trialMetricStyles = this.chart.metricStyles[this.marker];
+		newCircle.attr(trialMetricStyles);
+	}
+
+	return newCircle;
+}
+
+Metric.prototype.drawLine = function(y) {
+	// draw line on chart
+	var linePath = 'M '+(this.x-this.radius)+' '+y+' L '+(this.x+this.radius)+' '+y;
+	var newLine = this.chart.paper.path(linePath);
+	// set floor attributes onto line
+	var floorMetricStyles = this.chart.metricStyles[this.marker];
+	newLine.attr(floorMetricStyles);
+
+	return newLine;
+}
+
+Metric.prototype.drawCross = function(y) {
+	// draw cross on chart, cross is drawn as two lines
+	var crossPathOne = 'M '+(this.x-this.radius)+' '+(y-this.radius)+' L '+(this.x+this.radius)+' '+(y+this.radius);
+	var crossPathTwo = 'M '+(this.x-this.radius)+' '+(y+this.radius)+' L '+(this.x+this.radius)+' '+(y-this.radius);
+	var crossLineOne = this.chart.paper.path(crossPathOne);
+	var crossLineTwo = this.chart.paper.path(crossPathTwo);
+
+	// set error attributes onto cross
+	var errorMetricStyles = this.chart.metricStyles[this.marker];
+	crossLineOne.attr(errorMetricStyles);
+	crossLineTwo.attr(errorMetricStyles);
+
+	// create an array (Raphael set) that contains the error marker (two separate lines)
+	var errorSet = this.chart.paper.set();
+	errorSet.push(crossLineOne);
+	errorSet.push(crossLineTwo);
+
+	return errorSet;
+}
+
+Metric.prototype.changeMarkerValue = function(delta) {
+	if (this.type === 'floor') {
+		this.prevLogarithmicValue = this.logarithmicValue;
+		var newDelta;
+		if (delta > 0) {
+			if (this.prevLogarithmicValue >= 1) newDelta = delta;
+			if (this.prevLogarithmicValue >= .1 && this.prevLogarithmicValue < 1) newDelta = delta/10;
+			if (this.prevLogarithmicValue >= .01 && this.prevLogarithmicValue < .1) newDelta = delta/100;
+			if (this.prevLogarithmicValue >= .001 && this.prevLogarithmicValue < .01) newDelta = delta/100;
+		}
+		else if (delta < 0) {
+			if (this.prevLogarithmicValue > 1) newDelta = delta;
+			if (this.prevLogarithmicValue > .101 && this.prevLogarithmicValue <= 1) newDelta = delta/10;
+			if (this.prevLogarithmicValue > .011 && this.prevLogarithmicValue <= .101) newDelta = delta/100;
+			if (this.prevLogarithmicValue > .001 && this.prevLogarithmicValue <= .011) newDelta = delta/100;
+		}
+		console.log(newDelta);
+		this.logarithmicValue += newDelta;
+		this.value = Math.round(60/(this.logarithmicValue));
+	}
+	else {
+		this.prevLogarithmicValue = this.logarithmicValue;
+		this.logarithmicValue += delta;
+		this.value += delta;
+	}
+}
+
+Metric.prototype.changeMarkerPosition = function() {
+	this.y = this.getMarkerYPosition(this.prevLogarithmicValue);
+	console.log('previous y: ' + this.y);
+	var newYPos = this.calculateNewYPosition();
+	var moveDistance = -(this.y - newYPos);
+	console.log('new y: ' + newYPos);
+	// the errors marker is an array of two Raphael lines and must do transformation on each line individually
+	if (this.type === 'errors') {
+		this.paperObject[0].transform('...t0,' + moveDistance);
+		this.paperObject[1].transform('...t0,' + moveDistance);
+	}
+	else {
+		this.paperObject.transform('...t0,' + moveDistance);
+	}
+}
+
+Metric.prototype.calculateNewYPosition = function() {
+	var decadeBasePosition = this.chart.getDecadeBasePosition(this.decadeNumber);
+	var decadeBaseValue = this.chart.getDecadeBaseValue(this.decadeNumber);
+	var markerY = this.chart.valueToYPosition(decadeBasePosition, this.logarithmicValue * decadeBaseValue, decadeBaseValue);
+	if (this.type === "floors") {
+		return markerY + this.radius;
+	}
+	else {
+		return markerY;
+	}
+}
+
+Metric.prototype.changeValueAndMarker = function(delta) {
+	if (this !== null) {
+		this.changeMarkerValue(delta);
+		if (this.type === 'floor') {
+			$('#'+this.type).val(this.value+'"');
+		}
+		else {
+			$('#'+this.type).val(this.value);
+		}
+		this.changeMarkerPosition();
+	}
+}
+
+Metric.prototype.highlight = function() {
+	// ...
+}
